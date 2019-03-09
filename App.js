55834fb5fe920f9f52b8fe355551c317ab04a2d6
Eduardo Cervan <eduardo.cervan@jbtec.com.br>
2019-03-09 13:56:27 -0300

    Primeiro PUSH

---
+++ App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, Alert, Platform } from 'react-native';
+import Expo, { Constants } from 'expo';
+
+
+export default class App extends Component {
+  
+  state = {
+    compatible: false,
+    fingerprints: false,
+    result: ''
+  }
+  
+  componentDidMount() {
+    this.checkDeviceForHardware();
+    this.checkForFingerprints();
+  }
+  
+  checkDeviceForHardware = async () => {
+    let compatible = await Expo.Fingerprint.hasHardwareAsync();
+    this.setState({compatible})
+  }
+  
+  checkForFingerprints = async () => {
+    let fingerprints = await Expo.Fingerprint.isEnrolledAsync();
+    this.setState({fingerprints})
+  }
+  
+  scanFingerprint = async () => {
+   let result = await Expo.Fingerprint.authenticateAsync('Scan your finger.');
+   console.log('Scan Result:', result)
+  }
+  
+  showAndroidAlert = () => {
+    Alert.alert(
+      'Fingerprint Scan',
+      'Place your finger over the touch sensor and press scan.',
+      [
+        {text: 'Scan', onPress: () => {
+          this.scanFingerprint();
+        }},
+        {text: 'Cancel', onPress: () => console.log('Cancel'), style: 'cancel'}
+      ]
+    )
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+        Compatible Device? {this.state.compatible === true ? 'True' : 'False' }
+        </Text>
+        <Text style={styles.text}>
+        Fingerprings Saved? {this.state.fingerprints === true ? 'True' : 'False'}
+        </Text>
+        <TouchableOpacity onPress={Platform.OS === 'android' ? this.showAndroidAlert : this.scanFingerprint} style={styles.button}>
+          <Text style={styles.buttonText}>
+            SCAN
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 150,
+    height: 60,
+    backgroundColor: '#056ecf',
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 30,
+    color: '#fff'   
+  }
+});
